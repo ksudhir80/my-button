d3baf06bdf5b78de04d7b8eaea41226df66e5790
ksudhir <sudhir.kunnure@gmail.com>
2025-03-24 15:33:21 +0530

    inital code

---
+++ MyButton.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MyButton = ({ label }) => {
+  return <button style={{ padding: "10px", fontSize: "16px" }}>{label}</button>;
+};
+export default MyButton;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# my-button
+# MyButton
